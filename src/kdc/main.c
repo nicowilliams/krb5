bdd245eb404eacadc0e2c8284f9b088df4ba4cf3
Nicolas Williams <nico@cryptonector.com>
2012-05-31 17:15:15 -0500

    Update usage message of krb5kdc and the manpage to document inetd mode

--- src/kdc/main.c
+++ src/kdc/main.c
@@ -666,7 +666,7 @@ usage(char *name)
             _("usage: %s [-x db_args]* [-d dbpathname] [-r dbrealmname]\n"
               "\t\t[-R replaycachename] [-m] [-k masterenctype]\n"
               "\t\t[-M masterkeyname] [-p port] [-P pid_file]\n"
-              "\t\t[-n] [-w numworkers] [/]\n\n"
+              "\t\t[-i] [-n] [-w numworkers] [/]\n\n"
               "where,\n"
               "\t[-x db_args]* - Any number of database specific arguments.\n"
               "\t\t\tLook at each database module documentation for "

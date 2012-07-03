c12c8f8a5e74cda7132595b1d74277f36cddd5da
Nicolas Williams <nico@cryptonector.com>
2012-07-03 12:00:41 -0500

    Test keygen_enctypes

---
+++ src/tests/t_keygen_enctypes.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+from k5test import *
+import re
+
+# Run "kvno server" with a fresh set of client tickets, then check that the
+# enctypes in the service ticket match the expected values.
+re = re.compile(r'Unsupported encryption type while randomizing key')
+
+krb5_conf1 = {'all': {'libdefaults': {
+            'supported_enctypes': 'aes256-cts'}}}
+
+realm = K5Realm(krb5_conf=krb5_conf1, create_host=False, get_creds=False)
+realm.run_kadminl('addprinc -randkey -e aes256-cts:normal server')
+
+# Test with one-enctype keygen_enctypes
+realm.run_kadminl('setstr server keygen_enctypes aes256-cts:normal')
+realm.run_kadminl('getprinc server')
+output = realm.run_kadminl('cpw -randkey -e aes128-cts:normal server')
+if not re.search(output):
+    fail('keygen_enctypes policy not applied properly')
+realm.run_kadminl('getprinc server')
+output = realm.run_kadminl('cpw -randkey -e aes256-cts:normal server')
+if re.search(output):
+    fail('keygen_enctypes policy not applied properly')
+realm.run_kadminl('getprinc server')
+
+# Now test a multi-enctype keygen_enctypes.  Test that subsets are allowed,
+# the the complete set is allowed, that order doesn't matter, and that
+# enctypes outside the set are not allowed.
+realm.run_kadminl('setstr server keygen_enctypes aes256-cts:normal,rc4-hmac:normal')
+output = realm.run_kadminl('cpw -randkey -e rc4-hmac:normal server')
+if re.search(output):
+    fail('keygen_enctypes policy not applied properly')
+realm.run_kadminl('getprinc server')
+output = realm.run_kadminl('cpw -randkey -e aes256-cts:normal server')
+if re.search(output):
+    fail('keygen_enctypes policy not applied properly')
+realm.run_kadminl('getprinc server')
+output = realm.run_kadminl('cpw -randkey -e rc4-hmac:normal,aes256-cts:normal server')
+if re.search(output):
+    fail('keygen_enctypes policy not applied properly')
+realm.run_kadminl('getprinc server')
+output = realm.run_kadminl('cpw -randkey -e aes256-cts:normal,rc4-hmac:normal server')
+if re.search(output):
+    fail('keygen_enctypes policy not applied properly')
+realm.run_kadminl('getprinc server')
+output = realm.run_kadminl('cpw -randkey -e rc4-hmac:normal,aes128-cts:normal server')
+if not re.search(output):
+    fail('keygen_enctypes policy not applied properly')
+realm.run_kadminl('getprinc server')
+output = realm.run_kadminl('cpw -randkey -e rc4-hmac:normal,aes256-cts:normal,aes128-cts:normal server')
+if not re.search(output):
+    fail('keygen_enctypes policy not applied properly')
+realm.run_kadminl('getprinc server')
+realm.stop()
+
+success('keygen_enctypes')

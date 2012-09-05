db753c05bda3ec1057ae93ce15c04e48ab5ff99a
Nicolas Williams <nico@cryptonector.com>
2012-09-05 10:23:42 -0500

    start_kadmind() -> use the -p, -C, and -F options

--- src/util/k5test.py
+++ src/util/k5test.py
@@ -895,7 +895,12 @@ class K5Realm(object):
     def start_kadmind(self):
         global krb5kdc
         assert(self._kadmind_proc is None)
-        self._kadmind_proc = _start_daemon([kadmind, '-nofork', '-W'],
+        self._kadmind_proc = _start_daemon([kadmind, '-nofork', '-W',
+                                            '-p', kdb5_util,
+                                            '-C', kprop,
+                                            '-F',
+                                            os.path.join(self.testdir,
+                                                    'master-dump')],
                                             self.env_master, 'starting...')
 
     def stop_kadmind(self):
@@ -906,10 +911,15 @@ class K5Realm(object):
     def start_kpropd(self):
         global krb5kdc
         assert(self._kpropd_proc is None)
-        self._kpropd_proc = _start_daemon([kpropd, '-d', '-D', '-P', os.environ['KPROP_PORT'],
-                                           '-f', os.path.join(self.testdir, 'incoming-slave-datatrans'),
-                                           '-p', kdb5_util, '-a', os.path.join(self.testdir, 'kpropd-acl')],
-                                            self.env_slave, 'waiting for a kprop connection')
+        self._kpropd_proc = _start_daemon([kpropd, '-d', '-D', '-P',
+                                           os.environ['KPROP_PORT'],
+                                           '-f', os.path.join(self.testdir,
+                                               'incoming-slave-datatrans'),
+                                           '-p', kdb5_util, '-a',
+                                           os.path.join(self.testdir,
+                                               'kpropd-acl')],
+                                           self.env_slave,
+                                           'waiting for a kprop connection')
 
     def stop_kpropd(self):
         assert(self._kpropd_proc is not None)

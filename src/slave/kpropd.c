69af0b11bf340e0e7c9c740eccff6ebb01927a80
Nicolas Williams <nico@cryptonector.com>
2012-09-21 22:10:39 -0500

    Send kpropd debug msgs to stderr

--- src/slave/kpropd.c
+++ src/slave/kpropd.c
@@ -480,7 +480,7 @@ void doit(int fd)
                     host, sizeof(host), NULL, 0, 0) == 0) {
         syslog(LOG_INFO, _("Connection from %s"), host);
         if (debug)
-            printf("Connection from %s\n", host);
+            fprintf(stderr, "Connection from %s\n", host);
     }
 
     /*
@@ -1360,7 +1360,8 @@ kerberos_authenticate(context, fd, clientp, etype, my_sin)
             exit(1);
         }
 
-        printf(_("authenticated client: %s (etype == %s)\n"), name, etypebuf);
+        fprintf(stderr, _("authenticated client: %s (etype == %s)\n"),
+                name, etypebuf);
         free(name);
     }
 
@@ -1647,7 +1648,7 @@ load_database(context, kdb_util, database_file_name)
     kdb_log_context *log_ctx;
 
     if (debug)
-        printf("calling kdb5_util to load database\n");
+        fprintf(stderr, "calling kdb5_util to load database\n");
 
     log_ctx = context->kdblog_context;
 
@@ -1701,7 +1702,7 @@ load_database(context, kdb_util, database_file_name)
         /*NOTREACHED*/
     default:
         if (debug)
-            printf("Child PID is %d\n", child_pid);
+            fprintf(stderr, "Load PID is %d\n", child_pid);
         if (wait(&waitb) < 0) {
             com_err(progname, errno, _("while waiting for %s"), kdb_util);
             exit(1);

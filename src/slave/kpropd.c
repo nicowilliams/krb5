fc805aeb1349e28e8959897fab847b476cdc7d44
Nicolas Williams <nico@cryptonector.com>
2012-09-24 16:15:22 -0500

    Retab kpropd.c

--- src/slave/kpropd.c
+++ src/slave/kpropd.c
@@ -323,7 +323,7 @@ main(argc, argv)
         com_err(progname, errno, _("do_iprop failed.\n"));
         break;
     case 0:
-	do_standalone(pipefds[1]);
+        do_standalone(pipefds[1]);
         /* do_standalone() should never return */
         /* NOTREACHED */
         break;
@@ -694,7 +694,7 @@ wait_for_fullprop(int fd, time_t start_time, int start_timeout,
             return 0;
         }
         if (report.prop_progress && !report.prop_complete &&
-	    report.prop_time >= start_time)
+            report.prop_time >= start_time)
             alarm(progress_timeout); /* xfer started or making progress */
     } while (!report.load_complete || report.prop_time < start_time);
 
@@ -829,7 +829,7 @@ reinit:
      */
     if (debug) {
         fprintf(stderr, _("Initializing kadm5 as client %s\n"),
-		iprop_svc_princstr);
+                iprop_svc_princstr);
     }
     retval = kadm5_init_with_skey(kpropd_context, iprop_svc_princstr,
                                   srvtab,
@@ -899,7 +899,7 @@ reinit:
 
     for (;;) {
         int rvret;
-	struct timeval iprop_start, iprop_end;
+        struct timeval iprop_start, iprop_end;
         uint64_t us;
 
         incr_ret = NULL;
@@ -920,7 +920,7 @@ reinit:
 
         if (debug)
             fprintf(stderr, _("Calling iprop_get_updates_1()\n"));
-	gettimeofday(&iprop_start, NULL);
+        gettimeofday(&iprop_start, NULL);
         incr_ret = iprop_get_updates_1(&mylast, handle->clnt);
         if (incr_ret == (kdb_incr_result_t *)NULL) {
             clnt_perror(handle->clnt,
@@ -1052,7 +1052,7 @@ reinit:
                 fprintf(stderr, _("Incremental updates: %d updates / "
                                   "%llu us\n"),
                         incr_ret->updates.kdb_ulog_t_len, us);
-	    }
+            }
             break;
 
         case UPDATE_PERM_DENIED:
@@ -1453,7 +1453,7 @@ kerberos_authenticate(context, fd, clientp, etype, my_sin)
     }
 
     retval = krb5_recvauth(context, &auth_context, (void *) &fd,
-			   kprop_version, server, 0, keytab, &ticket);
+                           kprop_version, server, 0, keytab, &ticket);
     if (retval) {
         syslog(LOG_ERR, "Error in krb5_recvauth: %s",
                error_message(retval));

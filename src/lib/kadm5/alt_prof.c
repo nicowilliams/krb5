e230470f4d125e2cc26ffea5fed6962910f4927c
Nicolas Williams <nico@cryptonector.com>
2012-09-24 21:09:17 -0500

    Improve kpropd behavior in iprop mode
     - Make kpropd in iprop mode fork a child to listen for kprops from the
       master.  The child writes progress and outcome reports to the parent
       for each kprop.  This fixes a race between asking for a full resync
       and setting up a listener socket for it.
    
     - Add runonce (-t) for kpropd do_standalone() too.
    
     - Make kpropd log stats for iprop.  This is useful for sizing ulogs.
    
     - Improve debug output.
    
     - Add new iprop parameters iprop_dump_timeout and
       iprop_full_progress_timeout.

--- src/lib/kadm5/alt_prof.c
+++ src/lib/kadm5/alt_prof.c
@@ -802,6 +802,15 @@ krb5_error_code kadm5_get_config_params(context, use_kdc_config,
     GET_PORT_PARAM(iprop_port, KADM5_CONFIG_IPROP_PORT,
                    KRB5_CONF_IPROP_PORT, 0);
 
+    /* 5 min for large KDBs */
+    GET_DELTAT_PARAM(iprop_dump_timeout, KADM5_CONFIG_IPROP_DUMP_TIMEOUT,
+                     KRB5_CONF_IPROP_DUMP_TIMEOUT, 60 * 5);
+
+    GET_DELTAT_PARAM(iprop_progress_timeout,
+                     KADM5_CONFIG_IPROP_PROGRESS_TIMEOUT,
+                     KRB5_CONF_IPROP_PROGRESS_TIMEOUT, 10); /* 10 secs */
+
+
     hierarchy[2] = KRB5_CONF_IPROP_MASTER_ULOGSIZE;
 
     params.iprop_ulogsize = DEF_ULOGENTRIES;

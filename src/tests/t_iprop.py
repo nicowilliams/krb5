b238ee377959a72abbcb9afa4895c46f13582edf
Nicolas Williams <nico@cryptonector.com>
2012-10-02 17:42:32 -0500

    Code review comments re: t_iprop.py (part 2)

--- src/tests/t_iprop.py
+++ src/tests/t_iprop.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
 
-# XXX comment here
-kprop_port = '61010'
 import os
-os.environ['KPROP_PORT'] = kprop_port
+import time
 
 from k5test import *
 
@@ -23,7 +21,7 @@ iprop_kdc_conf = {
                 }}}
 }
 
-realm = K5Realm(kdc_conf=iprop_kdc_conf, create_user=False)
+realm = K5Realm(kdc_conf=iprop_kdc_conf, create_user=False, start_kadmind=True)
 
 ulog = os.path.join(realm.testdir, 'db.ulog')
 if not os.path.exists(ulog):
@@ -40,14 +38,8 @@ realm.run_as_master([kdb5_util, 'dump', dumpfile])
 realm.run_as_slave([kdb5_util, 'load', dumpfile])
 realm.run_as_slave([kdb5_util, 'stash', '-P', 'master'])
 
-## For debugging comment this and uncomment the subsequent lines.
-realm.start_kadmind()
-#realm.run_as_master(['/bin/bash', '-c', ' '.join([kadmind, '-nofork',
-#                     '-p', kdb5_util, '-F',
-#                     os.path.join(realm.testdir, 'master-dump'),
-#                     '>>' + os.path.join(realm.testdir, 'kadmind5.log'),
-#                     '2>&1', '&' ])])
-realm.run_as_slave(['/bin/sleep', '15'])
+output('Sleeping for 15 seconds\n')
+time.sleep(15)
 
 # Make some changes to the master db.
 realm.addprinc('wakawaka')
@@ -61,8 +53,8 @@ realm.addprinc('w')
 realm.run_kadminl('modprinc -allow_tix w')
 realm.run_kadminl('modprinc +allow_tix w')
 
-output = realm.run_as_master([kproplog, '-h'])
-if 'Last serial # : 7' not in output:
+out = realm.run_as_master([kproplog, '-h'])
+if 'Last serial # : 7' not in out:
     fail('Update log on master has incorrect last serial number.')
 
 # Set up the kpropd acl file.
@@ -80,8 +72,8 @@ realm.start_kpropd()
 #                    '>' + os.path.join(realm.testdir, 'kpropd-slave.log'),
 #                    '2>&1', '&' ])])
 realm.run_kadminl('modprinc -allow_tix w')
-output = realm.run_as_master([kproplog, '-h'])
-if 'Last serial # : 8' not in output:
+out = realm.run_as_master([kproplog, '-h'])
+if 'Last serial # : 8' not in out:
     fail('Update log on master has incorrect last serial number.')
 
 # We need to give iprop (really, a full resync here and maybe an
@@ -90,65 +82,70 @@ if 'Last serial # : 8' not in output:
 # Sometimes we need to wait a long time because kpropd's do_iprop()
 # can race with kadmind and fail to kadm5 init, which leads -apparently-
 # to some backoff effect.
-realm.run_as_slave(['/bin/sleep', '35'])
+output('Sleeping for 35 seconds\n')
+time.sleep(35)
 
 # Now check that iprop happened.  Note that we depend on timing here,
 # thus the above sleep, but there's no way to wait synchronously or force
 # iprop to happen (since iprop here is a pull system) and then wait for
 # it synchronously.
-output = realm.run_as_slave([kproplog, '-h'])
-if 'Last serial # : 8' not in output:
+out = realm.run_as_slave([kproplog, '-h'])
+if 'Last serial # : 8' not in out:
     fail('Update log on slave has incorrect last serial number.')
 
 # Make another change.
 realm.run_kadminl('modprinc +allow_tix w')
-output = realm.run_as_master([kproplog, '-h'])
-if 'Last serial # : 9' not in output:
+out = realm.run_as_master([kproplog, '-h'])
+if 'Last serial # : 9' not in out:
     fail('Update log on master has incorrect last serial number.')
 
 # Check that we're at sno 9 on the slave side too.
-realm.run_as_slave(['/bin/sleep', '35'])
-output = realm.run_as_slave([kproplog, '-h'])
-if 'Last serial # : 9' not in output:
+output('Sleeping for 35 seconds\n')
+time.sleep(35)
+out = realm.run_as_slave([kproplog, '-h'])
+if 'Last serial # : 9' not in out:
     fail('Update log on slave has incorrect last serial number.')
 
 # Reset the ulog on the slave side to force a full resync to the slave.
 realm.run_as_slave([kproplog, '-R'])
-output = realm.run_as_slave([kproplog, '-h'])
-if 'Last serial # : None' not in output:
+out = realm.run_as_slave([kproplog, '-h'])
+if 'Last serial # : None' not in out:
     fail('Reset of update log on slave failed.')
-realm.run_as_slave(['/bin/sleep', '35'])
+output('Sleeping for 35 seconds\n')
+time.sleep(35)
 # Check that a full resync happened.
-output = realm.run_as_slave([kproplog, '-h'])
-if 'Last serial # : 9' not in output:
+out = realm.run_as_slave([kproplog, '-h'])
+if 'Last serial # : 9' not in out:
     fail('Update log on slave has incorrect last serial number.')
 
 # Make another change.
 realm.run_kadminl('modprinc +allow_tix w')
-output = realm.run_as_master([kproplog, '-h'])
-if 'Last serial # : 10' not in output:
+out = realm.run_as_master([kproplog, '-h'])
+if 'Last serial # : 10' not in out:
     fail('Update log on master has incorrect last serial number.')
 
-realm.run_as_slave(['/bin/sleep', '35'])
-output = realm.run_as_slave([kproplog, '-h'])
-if 'Last serial # : 10' not in output:
+output('Sleeping for 35 seconds\n')
+time.sleep(35)
+out = realm.run_as_slave([kproplog, '-h'])
+if 'Last serial # : 10' not in out:
     fail('Update log on slave has incorrect last serial number.')
 
 # Reset the ulog on the master side to force a full resync to all slaves.
 # XXX Note that we only have one slave in this test, so we can't really
 # test this.
 realm.run_as_master([kproplog, '-R'])
-output = realm.run_as_master([kproplog, '-h'])
-if 'Last serial # : None' not in output:
+out = realm.run_as_master([kproplog, '-h'])
+if 'Last serial # : None' not in out:
     fail('Reset of update log on master failed.')
 realm.run_kadminl('modprinc -allow_tix w')
-output = realm.run_as_master([kproplog, '-h'])
-if 'Last serial # : 1' not in output:
+out = realm.run_as_master([kproplog, '-h'])
+if 'Last serial # : 1' not in out:
     fail('Update log on master has incorrect last serial number.')
-realm.run_as_slave(['/bin/sleep', '35'])
+output('Sleeping for 35 seconds\n')
+time.sleep(35)
 # Check that a full resync happened.
-output = realm.run_as_slave([kproplog, '-h'])
-if 'Last serial # : 1' not in output:
+out = realm.run_as_slave([kproplog, '-h'])
+if 'Last serial # : 1' not in out:
     fail('Update log on slave has incorrect last serial number.')
 
 success('iprop tests.')
